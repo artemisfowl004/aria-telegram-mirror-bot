7b636efd3bb58f35325a4387db18291f0231fa2f
artemisfowl004 <69702795+artemisfowl004@users.noreply.github.com>
2021-02-28 17:02:17 +0530

    Update .constants.js

--- src/.constants.js
+++ src/.constants.js
@@ -1,18 +1,18 @@
 module.exports = Object.freeze({
-  TOKEN: 'bot_token',
+  TOKEN: '1568517767:AAHCYtHr0Snv7Jphl-3QqBFcvdBKA7qgtbA',
   ARIA_SECRET: 'myaria2',
-  ARIA_DOWNLOAD_LOCATION: '/home/user/path/to/download/dir (no trailing "/")',
+  ARIA_DOWNLOAD_LOCATION: '/home/user/path/to/download/dir',
   ARIA_DOWNLOAD_LOCATION_ROOT: '/', //The mountpoint that contains ARIA_DOWNLOAD_LOCATION
-  ARIA_FILTERED_DOMAINS: ['yts', 'YTS', 'cruzing.xyz', 'eztv.ag', 'YIFY'], // Prevent downloading from URLs containing these substrings
-  ARIA_FILTERED_FILENAMES: ['YIFY'], // Files/top level directories with these substrings in the filename won't be downloaded
+  ARIA_FILTERED_DOMAINS: [], // Prevent downloading from URLs containing these substrings
+  ARIA_FILTERED_FILENAMES: [], // Files/top level directories with these substrings in the filename won't be downloaded
   ARIA_PORT: 8210, // Port for aria2c RPC server, if you change this here, make sure to update aria.sh as well
-  GDRIVE_PARENT_DIR_ID: 'id_of_Drive_folder_to_upload_into',
+  GDRIVE_PARENT_DIR_ID: '0AFjX8c8S5_BFUk9PVA',
   OTHER_GDRIVE_DIR_IDS: [], // This is needed if u want to look for files in multiple dirs on list command
-  SUDO_USERS: [012, 345],	// Telegram user IDs. These users can use the bot in any chat.
-  AUTHORIZED_CHATS: [678, 901],	// Telegram chat IDs. Anyone in these chats can use the bot.
-  STATUS_UPDATE_INTERVAL_MS: 12000, // A smaller number will update faster, but might cause rate limiting
+  SUDO_USERS: [1645049777, 1426433009], // Telegram user IDs. These users can use the bot in any chat.
+  AUTHORIZED_CHATS: [678, 901], // Telegram chat IDs. Anyone in these chats can use the bot.
+  STATUS_UPDATE_INTERVAL_MS: 5000, // A smaller number will update faster, but might cause rate limiting
   DRIVE_FILE_PRIVATE: {
-    ENABLED: false,
+    ENABLED: true,
     EMAILS: ['someMail@gmail.com', 'someOtherMail@gmail.com']
   },
   DOWNLOAD_NOTIFY_TARGET: {  // Information about the web service to notify on download completion.
@@ -25,8 +25,8 @@ module.exports = Object.freeze({
     ENABLED: false,  // If true, all commands except '/list' has to have the bot username after the command
     NAME: "@nameOf_bot"
   },
-  IS_TEAM_DRIVE: false,
+  IS_TEAM_DRIVE: true,
   USE_SERVICE_ACCOUNT: false,
-  INDEX_DOMAIN: "",
-  TELEGRAPH_TOKEN: '' // Telegraph token, if you want to show search results in telegra.ph else keep it blank
+  INDEX_DOMAIN: "https://mirrorbot007.mirrorbot007.workers.dev/0:/",
+  TELEGRAPH_TOKEN: '9ea6cfb5ef856e7c8fb3fec675ec10f67d8fbe15d44ea178c1abc0ac249c' // Telegraph token, if you want to show search results in telegra$
 });
